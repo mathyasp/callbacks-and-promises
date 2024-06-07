801b90aa5b06887948584dde4bd16b27aadbe0eb
mathyasp <mathyastp@gmail.com>
2024-06-07 16:54:36 -0400

    promise 11

--- Promise-11/index.js
+++ Promise-11/index.js
@@ -18,9 +18,16 @@ console.log(greeting)
 // Test the code here. Pay close attention to the return value. 
 // What is returned? Is it the value returned by the function
 // or something else? Write your answer below:
-
+// The return value is a Promise object that include the value returned.
 
 // An async function always wraps the value it returns in a Promise. 
 // To get at the value you must resolve the Priomise. Do that here. 
 // Use `.then()` to resolve the Promises `data` and `greeting`:
 
+data.then(result => {
+  console.log(result)
+});
+
+greeting.then(result => {
+  console.log(result)
+});
